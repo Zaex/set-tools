0b47c7aeff26ef4ebb86fc676b673c27da809ced
SEDGWICK <alexandersedgwick@northwesternmutual.com>
2017-05-12 14:40:45 -0500

    Separated testing and refined namespace and directory structure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zaex.rel-tools "0.1.0-SNAPSHOT"
+(defproject zaex.rel-tools "0.1.0"
   :description "Extending clojure.set"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :min-lein-version "2.0.0"
