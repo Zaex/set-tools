aa914b5e931d63ab0877b6b6352cd4a422aa4670
Alex Sedgwick <ajsedgwick@gmail.com>
2019-09-02 15:16:08 -0500

    some small changes

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject zaex.rel-tools "0.1.0"
+(defproject zaex/rel-tools "0.1.0"
   :description "Extending clojure.set"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :min-lein-version "2.0.0"
-  :uberjar-name "rel-tools")
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.cgrand/xforms "0.18.2"]]
+  :min-lein-version "2.0.0")
--- src/zaex/rel_tools.clj
+++ src/zaex/rel_tools.clj
@@ -1,6 +1,6 @@
 (ns zaex.rel-tools
   "Most of the source has been shamelessly taken from clojure.set/join 
-and then enhanced with transients and 1-arty support."
+  and then enhanced with transients and 1-arty support."
   (:require [clojure.set :refer [intersection
                                  difference
                                  project
@@ -8,7 +8,8 @@ and then enhanced with transients and 1-arty support."
                                  rename-keys
                                  map-invert
                                  index
-                                 union]]))
+                                 union]]
+            #_[net.cgrand.xforms :as x]))
 
 
 (defn join
@@ -16,44 +17,43 @@ and then enhanced with transients and 1-arty support."
   can be used with transducers hence this has 1-arty support"
   ([rel] rel)
   ([xrel yrel]
-   (if (and (seq xrel) (seq yrel))
-     (let [ks (intersection (set (keys (first xrel))) (set (keys (first yrel))))
-           idx (index xrel ks)]
+   (let [ks (intersection (set (keys (first xrel)))
+                          (set (keys (first yrel))))]
+     (join xrel yrel (zipmap ks ks))))
+  ([xrel yrel km]
+   (if (and (set? xrel) (set? yrel))
+     (let [[r s k] (if (<= (count xrel) (count yrel))
+                     [xrel yrel (map-invert km)]
+                     [yrel xrel km]) 
+           idx (index r (vals k))]
        (persistent!
-         (reduce (fn [ret x]
-                   (if-let [found (idx (select-keys x ks))]
-                     (reduce #(conj! %1 (merge %2 x)) ret found)
-                     ret))
-                 (transient #{})
-                 yrel)))
-     #{}))
-  ([xrel yrel km] 
-   (let [k (map-invert km)
-         idx (index xrel (vals k))]
-     (persistent!
-       (reduce (fn [ret x]
-                 (if-let [found (idx (rename-keys (select-keys x (keys k)) k))]
-                   (reduce #(conj! %1 (merge %2 x)) ret found)
-                   ret))
-               (transient #{})
-               yrel)))))
+        (reduce (fn [ret x]
+                  (if-let [found (idx (rename-keys (select-keys x (keys k)) k))]
+                    (reduce #(conj! %1 (merge %2 x)) ret found)
+                    ret))
+                (transient #{})
+                s))))))
+       
+
 
 (defn left-join
   "Can be used with transducers as it has 1-arty support"
   ([rel] rel)
-  ([xrel yrel] 
-   (if (and (seq xrel) (seq yrel))
-     (let [ks (intersection (set (keys (first xrel))) (set (keys (first yrel))))
-           idx (index yrel ks)]
-       (into #{}
-             (filter #(nil? (idx (select-keys % ks))))
-             xrel))
-     #{}))
-  ([xrel yrel km] 
-   (let [idx (index yrel (vals km))]
-     (into #{}
-           (filter #(nil? (idx (rename-keys (select-keys % (keys km)) km))))
-           xrel))))
+  ([xrel yrel]
+   (let [ks (intersection (set (keys (first xrel)))
+                          (set (keys (first yrel))))]
+     (left-join xrel yrel (zipmap ks ks))))
+  ([xrel yrel km]
+   (if (and (set? xrel) (set? yrel))
+     (let [k (map-invert km)
+           idx (index yrel (vals km))]
+       (persistent!
+        (reduce (fn [ret x]
+                  (if-let [found (idx (rename-keys (select-keys x (keys k)) k))]
+                    (reduce #(conj! %1 (merge %2 x)) ret found)
+                    (conj! ret x)))
+                (transient #{})
+                xrel))))))
 
 (defn right-join
   ([rel] rel)
